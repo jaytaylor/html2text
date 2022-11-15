a51d02abf412f5d295a106c28f202f0bdec57ee5
Jonathan Hall <flimzy@flimzy.com>
2022-11-15 16:05:59 +0100

    Fix OmitLinks support

--- html2text.go
+++ html2text.go
@@ -267,7 +267,7 @@ func (ctx *textifyTraverseContext) handleElement(node *html.Node) error {
 		if attrVal := getAttrVal(node, "href"); attrVal != "" {
 			attrVal = ctx.normalizeHrefLink(attrVal)
 			// Don't print link href if it matches link element content or if the link is empty.
-			if (!ctx.options.OmitLinks && attrVal != "" && linkText != attrVal) || !ctx.options.TextOnly {
+			if (attrVal != "" && linkText != attrVal) && !ctx.options.OmitLinks && !ctx.options.TextOnly {
 				hrefLink = "( " + attrVal + " )"
 			}
 		}
